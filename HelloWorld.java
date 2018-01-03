4e39267f060a12d87f2df98f125849664c1dfaa1
MsBrownCS <krbrown@psdschools.org>
2018-01-03 08:43:27 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void hello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Kristina Brown, welcome to our class today.");
+
+	}
+
+}
